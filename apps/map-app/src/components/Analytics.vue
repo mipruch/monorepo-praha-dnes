eb05fd947dc1ccc6cc62b4e8658cdd8e7c5a53c7
Michal Průcha <michal.prucha80@gmail.com>
2024-03-04 14:29:03 +0100

    Card component, loader icon

--- apps/map-app/src/components/Analytics.vue
+++ apps/map-app/src/components/Analytics.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import {Plus, Minus, Trash2} from "lucide-vue-next";
+import {Plus} from "lucide-vue-next";
+import {Loader2} from "lucide-vue-next";
 import Button from "./ui/button/Button.vue";
 import {
 	DropdownMenu,
@@ -9,95 +10,73 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import Tags from "./Tags.vue";
-import Tag from "./Tag.vue";
 import {pragueMap} from "../stores/mapStore";
-import {onMounted, watch} from "vue";
+import {ref, watch} from "vue";
 import layers from "@/stores/preferences.json";
+import Card from "./Card.vue";
 
 function refresh() {
 	console.log(pragueMap.layers);
 }
+
+function addLayer(id: string) {
+	isLoading.value = true;
+	pragueMap.addLayer(id).then(() => {
+		isLoading.value = false;
+	});
+}
+
+const isLoading = ref(false);
 </script>
 
 <template>
 	<div class="bg-background rounded-[var(--gap)] p-7">
-		<DropdownMenu>
-			<DropdownMenuTrigger class="mb-12"
-				><Button>
-					<Plus class="mr-2" />
-					Přidat vrstvu</Button
-				></DropdownMenuTrigger
-			>
-			<DropdownMenuContent>
-				<DropdownMenuLabel>Veřejná správa</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem @click="pragueMap.addLayer('1')"
-					>Úřady</DropdownMenuItem
+		<div class="flex flex-row items-center mb-12">
+			<DropdownMenu>
+				<DropdownMenuTrigger class="mr-4 flex"
+					><Button>
+						<Plus class="mr-2" />
+						Přidat vrstvu</Button
+					></DropdownMenuTrigger
 				>
-				<DropdownMenuItem
-					v-for="{id, name} in layers"
-					@click="pragueMap.addLayer(id)"
-					:disabled="pragueMap.activeLayers.has(id)"
-					>{{ name }}</DropdownMenuItem
-				>
-				<DropdownMenuItem>Magistrát</DropdownMenuItem>
-				<DropdownMenuItem>Team</DropdownMenuItem>
-				<DropdownMenuItem>Subscription</DropdownMenuItem>
-				<DropdownMenuSeparator />
-				<DropdownMenuLabel>Kultura</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem>Divadla</DropdownMenuItem>
-				<DropdownMenuItem>Koncetry</DropdownMenuItem>
-				<DropdownMenuItem>Sport</DropdownMenuItem>
-				<DropdownMenuItem>Kino</DropdownMenuItem>
-				<DropdownMenuSeparator />
-				<DropdownMenuLabel>Doprava</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem @click="pragueMap.addLayer('2')"
-					>Parkoviště</DropdownMenuItem
-				>
-				<DropdownMenuItem>Koncetry</DropdownMenuItem>
-				<DropdownMenuItem>Sport</DropdownMenuItem>
-				<DropdownMenuItem>Kino</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-
-		<div
-			class="p-4 bg-grey-200 rounded-lg mb-8"
-			v-for="[key, value] in Array.from(pragueMap.activeLayers.entries())"
-		>
-			<div>
-				<div class="flex flex-row justify-between pb-4">
-					<Tags big v-slot="slotProps">
-						<Tag
-							color="green"
-							:text="value.pref.name"
-							:big="slotProps.big"
-						/>
-						<Tag
-							color="light"
-							:text="value.pref.name"
-							:big="slotProps.big"
-						/>
-					</Tags>
-					<Button
-						variant="ghost"
-						size="icon"
-						@click="pragueMap.removeLayer(key)"
+				<DropdownMenuContent>
+					<DropdownMenuLabel>Veřejná správa</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem
+						v-for="{id, name} in layers"
+						@click="addLayer(id)"
+						:disabled="pragueMap.activeLayers.has(id)"
+						>{{ name }}</DropdownMenuItem
 					>
-						<Trash2 />
-					</Button>
-				</div>
-				<div class="pl-1 mb-8">
-					{{
-						JSON.stringify(value.pref)
-							.split('"')
-							.join(" ")
-							.slice(0, 300)
-					}}
-				</div>
-			</div>
+					<DropdownMenuItem>Magistrát</DropdownMenuItem>
+					<DropdownMenuItem>Team</DropdownMenuItem>
+					<DropdownMenuItem>Subscription</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuLabel>Kultura</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>Divadla</DropdownMenuItem>
+					<DropdownMenuItem>Koncetry</DropdownMenuItem>
+					<DropdownMenuItem>Sport</DropdownMenuItem>
+					<DropdownMenuItem>Kino</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuLabel>Doprava</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem @click="pragueMap.addLayer('2')"
+						>Parkoviště</DropdownMenuItem
+					>
+					<DropdownMenuItem>Koncetry</DropdownMenuItem>
+					<DropdownMenuItem>Sport</DropdownMenuItem>
+					<DropdownMenuItem>Kino</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+			<Loader2 class="w-8 h-8 animate-spin" v-if="isLoading" />
 		</div>
+
+		<Card
+			v-for="[key, value] in Array.from(pragueMap.activeLayers.entries())"
+			:layer-id="key"
+			:layer-data="value"
+			@remove-layer="pragueMap.removeLayer(key)"
+		/>
 	</div>
 </template>
---
+++ apps/map-app/src/components/Card.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import Tag from "./Tag.vue";
+import Tags from "./Tags.vue";
+import Button from "./ui/button/Button.vue";
+import {Trash2} from "lucide-vue-next";
+
+defineEmits(["removeLayer"]);
+
+const {layerData, layerId} = defineProps<{
+	layerId: string;
+	layerData: {
+		pref: any;
+		res: any;
+	};
+}>();
+</script>
+
+<template>
+	<div class="p-4 bg-grey-200 rounded-lg mb-8">
+		<div>
+			<div class="flex flex-row justify-between pb-4">
+				<Tags big v-slot="slotProps">
+					<Tag
+						color="green"
+						:text="layerData.pref.name"
+						:big="slotProps.big"
+					/>
+					<Tag
+						color="light"
+						:text="layerData.pref.name"
+						:big="slotProps.big"
+					/>
+				</Tags>
+				<Button
+					variant="ghost"
+					size="icon"
+					@click="$emit('removeLayer')"
+				>
+					<Trash2 />
+				</Button>
+			</div>
+			<div class="pl-1 mb-8">
+				{{ layerData.pref.paragraph }}
+			</div>
+		</div>
+	</div>
+</template>
