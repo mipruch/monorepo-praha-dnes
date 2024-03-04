037d58acbd8b793a7ef5eeb6d1783f4e681d9b14
Michal Průcha <michal.prucha80@gmail.com>
2024-03-04 01:12:34 +0100

    UI, reactive list of layers

--- apps/map-app/src/components/Analytics.vue
+++ apps/map-app/src/components/Analytics.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import {Plus, Minus} from "lucide-vue-next";
-import Button from "./Button.vue";
+import {Plus, Minus, Trash2} from "lucide-vue-next";
+import Button from "./ui/button/Button.vue";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import Tags from "./Tags.vue";
 import Tag from "./Tag.vue";
 import {pragueMap} from "../stores/mapStore";
 import {onMounted, watch} from "vue";
+import layers from "@/stores/preferences.json";
 
 function refresh() {
 	console.log(pragueMap.layers);
@@ -13,27 +22,82 @@ function refresh() {
 
 <template>
 	<div class="bg-background rounded-[var(--gap)] p-7">
-		<Button @click="pragueMap.addLayer('Municipality')">
-			<Plus class="mr-2" />
-			Přidat vrstvu
-		</Button>
-		<Button @click="pragueMap.removeLayer('Municipality')">
-			<Minus class="mr-2" />
-			Odebrat vrstvu
-		</Button>
-		<Button @click="refresh"> refresh </Button>
-		<div class="text-orange text-3xl"></div>
-		<h1 class="">Officia ut elit esse officia sint culpa eu commodo</h1>
-		<h2>Officia ut elit esse officia sint culpa eu commodo</h2>
-		<h3>Officia ut elit esse officia sint culpa eu commodo</h3>
-		<h4>Officia ut elit esse officia sint culpa eu commodo</h4>
-		<h5>Officia ut elit esse officia sint culpa eu commodo</h5>
-		<h6>Officia ut elit esse officia sint culpa eu commodo</h6>
-		<p>Officia ut elit esse officia sint culpa eu commodo</p>
+		<DropdownMenu>
+			<DropdownMenuTrigger class="mb-12"
+				><Button>
+					<Plus class="mr-2" />
+					Přidat vrstvu</Button
+				></DropdownMenuTrigger
+			>
+			<DropdownMenuContent>
+				<DropdownMenuLabel>Veřejná správa</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem @click="pragueMap.addLayer('1')"
+					>Úřady</DropdownMenuItem
+				>
+				<DropdownMenuItem
+					v-for="{id, name} in layers"
+					@click="pragueMap.addLayer(id)"
+					:disabled="pragueMap.activeLayers.has(id)"
+					>{{ name }}</DropdownMenuItem
+				>
+				<DropdownMenuItem>Magistrát</DropdownMenuItem>
+				<DropdownMenuItem>Team</DropdownMenuItem>
+				<DropdownMenuItem>Subscription</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuLabel>Kultura</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>Divadla</DropdownMenuItem>
+				<DropdownMenuItem>Koncetry</DropdownMenuItem>
+				<DropdownMenuItem>Sport</DropdownMenuItem>
+				<DropdownMenuItem>Kino</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuLabel>Doprava</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem @click="pragueMap.addLayer('2')"
+					>Parkoviště</DropdownMenuItem
+				>
+				<DropdownMenuItem>Koncetry</DropdownMenuItem>
+				<DropdownMenuItem>Sport</DropdownMenuItem>
+				<DropdownMenuItem>Kino</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
 
-		<Tags big v-slot="slotProps">
-			<Tag color="green" text="Hello World" :big="slotProps.big" />
-			<Tag color="green" text="Hello World" :big="slotProps.big" />
-		</Tags>
+		<div
+			class="p-4 bg-grey-200 rounded-lg"
+			v-for="[key, value] in Array.from(pragueMap.activeLayers.entries())"
+		>
+			<div>
+				<div class="flex flex-row justify-between pb-4">
+					<Tags big v-slot="slotProps">
+						<Tag
+							color="green"
+							:text="value.pref.name"
+							:big="slotProps.big"
+						/>
+						<Tag
+							color="light"
+							:text="value.pref.name"
+							:big="slotProps.big"
+						/>
+					</Tags>
+					<Button
+						variant="ghost"
+						size="icon"
+						@click="pragueMap.removeLayer(key)"
+					>
+						<Trash2 />
+					</Button>
+				</div>
+				<div class="pl-1 mb-8">
+					{{
+						JSON.stringify(value.pref)
+							.split('"')
+							.join(" ")
+							.slice(0, 300)
+					}}
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
