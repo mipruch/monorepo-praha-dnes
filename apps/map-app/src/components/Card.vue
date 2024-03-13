c5d660333037dcb8376b5d36b92328f7378a182c
Michal Průcha <michal.prucha80@gmail.com>
2024-03-13 01:02:13 +0100

    Opening hours compatibility

--- apps/map-app/src/components/Card.vue
+++ apps/map-app/src/components/Card.vue
@@ -7,6 +7,11 @@ import WidgetMedium from "./WidgetMedium.vue";
 import WidgetSmall from "./WidgetSmall.vue";
 import WidgetLarge from "./WidgetLarge.vue";
 
+import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
+dayjs.locale("cs");
+dayjs.extend(isBetween);
+
 defineEmits(["removeLayer"]);
 const {layerData} = defineProps<{
 	layerData: {
@@ -16,13 +21,21 @@ const {layerData} = defineProps<{
 }>();
 
 type ValueDefinition = {
-	mathOperation: "sum" | "average" | "count";
+	mathOperation:
+		| "sum"
+		| "average"
+		| "countFeatures"
+		| "mostCommon"
+		| "countUnique";
 	attributePath:
 		| {
 				arrayPath: string;
 				where: {
 					path: string;
 					equals: string;
+					pathDay?: string;
+					pathTimeOpens?: string;
+					pathTimeCloses?: string;
 				};
 				valuePath: string;
 		  }
@@ -37,11 +50,13 @@ function handleGraph(graph: any): any {
 function handleValue(
 	value: string | number | ValueDefinition
 ): string | number {
+	// Pokud je value string nebo number, vrátíme ho bez žádných dalších operací
 	if (typeof value === "string" || typeof value === "number") {
 		return value;
 	}
 
-	if (value.mathOperation === "count") {
+	// Pokud je zapotřebí pouze spočítat počet feature, vrátíme bez dalších operací počet feature
+	if (value.mathOperation === "countFeatures") {
 		return layerData.res.features.length;
 	}
 
@@ -73,23 +88,25 @@ function extractValuesFromLayerData(value: ValueDefinition): any[] {
 		where: {path: string; equals: string};
 		valuePath: string;
 	};
-	const pathToAttribute = attributePath.arrayPath;
+
 	const arrayOfValues: any[] = [];
 
 	layerData.res.features.forEach((feature: any) => {
 		const arrayOfMeasurements = resolvePath(
 			feature.properties,
-			pathToAttribute
+			attributePath.arrayPath
 		);
 
-		arrayOfMeasurements.forEach((measurement: any) => {
+		arrayOfMeasurements.find((measurement: any) => {
 			if (matchesCondition(measurement, attributePath.where)) {
 				const foundValue = resolvePath(
 					measurement,
 					attributePath.valuePath
 				);
 				arrayOfValues.push(foundValue);
+				return true;
 			}
+			return false;
 		});
 	});
 	return arrayOfValues;
@@ -97,12 +114,10 @@ function extractValuesFromLayerData(value: ValueDefinition): any[] {
 
 function performMathOperation(
 	values: any[],
-	operation: "sum" | "average" | "mostCommon"
+	operation: "sum" | "count" | "average" | "mostCommon" | "countUnique"
 ): number | string {
-	const sum = values.reduce((a: any, b: any) => a + b, 0);
-
-	if (operation === "average") {
-		return (values.length ? sum / values.length : 0) as number;
+	if (operation === "count") {
+		return values.length;
 	}
 
 	if (operation === "mostCommon") {
@@ -116,28 +131,73 @@ function performMathOperation(
 		return mostCommon;
 	}
 
+	if (operation === "countUnique") {
+		return new Set(values).size;
+	}
+
+	const sum = values.reduce((a: any, b: any) => a + b, 0);
+
+	if (operation === "average") {
+		return (values.length ? sum / values.length : 0) as number;
+	}
+
 	return sum as number;
 }
 
 function matchesCondition(
 	component: any,
-	where: {path: string; equals: string}
+	where: {
+		path: string;
+		equals: string;
+		pathDay?: string;
+		pathTimeOpens?: string;
+		pathTimeCloses?: string;
+	}
 ): boolean {
 	const value = resolvePath(component, where.path);
+
+	// Kontrole, zda dnes otevřeno
+	if (where.equals === "OPEN_TODAY" && value === dayjs().format("dddd")) {
+		return true;
+	}
+
+	// Kontrole, zda nyní otevřeno
+	if (
+		where.equals === "OPEN_NOW" &&
+		where.pathDay &&
+		where.pathTimeOpens &&
+		where.pathTimeCloses
+	) {
+		const day = resolvePath(component, where.pathDay);
+		const now = dayjs();
+
+		if (day !== now.format("dddd")) {
+			return false;
+		}
+
+		const opens = resolvePath(component, where.pathTimeOpens);
+		const closes = resolvePath(component, where.pathTimeCloses);
+
+		const opensTime = dayjs(now.format("YYYY.MM.DD") + " " + opens);
+		const closesTime = dayjs(now.format("YYYY.MM.DD") + " " + closes);
+
+		return now.isBetween(opensTime, closesTime);
+	}
+
+	// where.equals nemá speciální (dynamickou / časovou) hodnotu, takže se porovnává přímo.
 	return value === where.equals;
 }
 
 function resolvePath(object: any, path: string): any {
+	if (!path) return object;
 	const pathArr = path.split(".") || [];
 	return pathArr.reduce((acc: any, curr: string) => acc[curr], object);
 }
 
 function formatNumber(number: number, decimals: number): string {
-	// Then, format the number with space as a thousands separator
 	return new Intl.NumberFormat("cs-CZ", {
 		minimumFractionDigits: 0,
 		maximumFractionDigits: 1,
-		// Use ' ' (space) as the thousands separator. Adjust the locale and options as needed.
 	}).format(number);
 }
 </script>
