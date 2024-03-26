64ff4db5fbfdc1ee8466e91ca637f0ede7dda3e7
Michal Průcha <michal.prucha80@gmail.com>
2024-03-26 14:27:00 +0100

    monaco editor, correct api link map-app

--- apps/cms/pages/[id].vue
+++ apps/cms/pages/[id].vue
@@ -1,16 +1,53 @@
 <script setup lang="ts">
+// import * as monaco from "monaco-editor";
+
 const route = useRoute();
 
 const layers = await fetch(
 	`https://abuz6lqd47.execute-api.eu-central-1.amazonaws.com/prod/layers/${route.params.id}`
-).then((r) => r.json());
+).then((r) => r.text());
+
+const value = ref("function hello() {\n\talert('Hello world!');\n}");
+value.value = layers;
+
+const editorRef = ref();
+
+// For example, add greeting action to editor...
+onMounted(() => {
+	watch(
+		() => editorRef.value.$editor,
+
+		() => {
+			function formatDocument() {
+				editorRef.value.$editor
+					.getAction("editor.action.formatDocument")
+					.run();
+			}
+
+			console.log("now");
+			editorRef.value.$editor.onDidChangeModelLanguageConfiguration(
+				formatDocument
+			);
+
+			/// on every initialization
+			editorRef.value.$editor.onDidLayoutChange(formatDocument);
+		}
+	);
+});
 </script>
 <template>
 	<div>
 		<h1>Praha dnes CMS</h1>
 		<h3>Seznam vrstev</h3>
-		<div>
-			{{ layers }}
+		<div style="border: black 2px solid; padding: 0.5rem; height: 600px">
+			<MonacoEditor
+				v-model="value"
+				lang="json"
+				style="height: 100%"
+				ref="editorRef"
+				:options="{theme: 'vs-dark'}"
+			/>
 		</div>
+		{{ value }}
 	</div>
 </template>
